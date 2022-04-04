592500e015db71de5ef0fbecca28e3f368d87fe8
MirkoPele <96829932+MirkoPele@users.noreply.github.com>
2022-04-04 13:32:32 +0200

    Pridanie funkcie na talcitko
    Po stlaceni tlacidla Start sa otvori nova scena kde sa neskor bude zobrazovat priebeh hry

--- src/main/java/app/minesweeper/Controller.java
+++ src/main/java/app/minesweeper/Controller.java
@@ -1,20 +1,26 @@
 package app.minesweeper;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuBar;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
     private AnchorPane backPanel;
     @FXML
@@ -34,6 +40,19 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 
+
+    /* handling buttons clicks */
+    public void handleButtonAction(ActionEvent actionEvent) throws IOException {
+
+        /* open scene with game when startButton is clicked */
+        if(actionEvent.getSource()==startButton){
+            Parent root = FXMLLoader.load(getClass().getResource("gameView.fxml"));
+            stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 }
