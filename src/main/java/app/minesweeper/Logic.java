0fff59826efc6f3e2fd588a3460064ec4e5efa07
jakubsepik <jakubsepik@gmail.com>
2022-04-10 20:27:24 +0200

    Update Logic.java

--- src/main/java/app/minesweeper/Logic.java
+++ src/main/java/app/minesweeper/Logic.java
@@ -1,13 +1,5 @@
 package app.minesweeper;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import app.minesweeper.GameControler;
-
 import java.util.*;
 
 public class Logic {
@@ -16,22 +8,23 @@ public class Logic {
     private final ArrayList<int[]> flags;
     private final char[][] board;
     Random rn = new Random();
+
     public Logic(int size) {
         this.size = size;
         this.flags = new ArrayList<>();
         board = new char[size][size];
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             char[] tmp = new char[size];
             Arrays.fill(tmp, (char) 48);
-            board[i]=tmp;
+            board[i] = tmp;
         }
 
-        mines = size*size/5;
+        mines = size * size / 5;
 
         for (byte i = 0; i < mines; i++) {
             int x = rn.nextInt(size);
             int y = rn.nextInt(size);
-            if (board[x][y]!='X') {
+            if (board[x][y] != 'X') {
                 board[x][y] = 'X';
 
                 if (x + 1 < board.length)
@@ -65,86 +58,77 @@ public class Logic {
                 if (x - 1 >= 0 && y + 1 < board.length)
                     if (board[x - 1][y + 1] != 88)
                         board[x - 1][y + 1]++;
-            }else
+            } else
                 i--;
         }
-        for(char[] tmp:board){
+        for (char[] tmp : board) {
             System.out.println(Arrays.toString(tmp));
         }
     }
-    public List<int[]> getOne(int x,int y){
+
+    public List<int[]> getOne(int x, int y) {
         List<int[]> list = new ArrayList<>();
-        list.add(new int[]{x,y,board[x][y]});
-        if(board[x][y]!='0')
+        list.add(new int[]{x, y, board[x][y]});
+        if (board[x][y] != '0')
             return list;
         Queue<int[]> zeros = new LinkedList<>();
-        zeros.add(new int[]{x,y});
-
-        boolean zmena;
-        do{
-            zmena=false;
+        zeros.add(new int[]{x, y});
+        do {
             int zero_size = zeros.size();
-            for(int i=0;i<zero_size;i++){
+            for (int i = 0; i < zero_size; i++) {
                 int[] tmp = zeros.remove();
                 int xl = tmp[0];
                 int yl = tmp[1];
-
-                if (xl + 1 < size && !containsXY(list,xl+1,yl)) {
-                    list.add(new int[]{xl+1,yl,board[xl+1][yl]});
-                    if(board[xl+1][yl]=='0'){
-                        zeros.add(new int[]{xl+1,yl});
-                        zmena=true;
+                if (xl + 1 < size && notContainsXY(list, xl + 1, yl)) {
+                    list.add(new int[]{xl + 1, yl, board[xl + 1][yl]});
+                    if (board[xl + 1][yl] == '0') {
+                        zeros.add(new int[]{xl + 1, yl});
                     }
                 }
-
-                if (xl - 1 >= 0 && !containsXY(list,xl-1,yl)) {
-                    list.add(new int[]{xl-1,yl,board[xl-1][yl]});
-                    if(board[xl-1][yl]=='0'){
-                        zeros.add(new int[]{xl-1,yl});
-                        zmena=true;
+                if (xl - 1 >= 0 && notContainsXY(list, xl - 1, yl)) {
+                    list.add(new int[]{xl - 1, yl, board[xl - 1][yl]});
+                    if (board[xl - 1][yl] == '0') {
+                        zeros.add(new int[]{xl - 1, yl});
                     }
                 }
-
-                if (yl + 1 < size && !containsXY(list,xl,yl+1)) {
-                    list.add(new int[]{xl,yl+1,board[xl][yl+1]});
-                    if(board[xl][yl+1]=='0'){
-                        zeros.add(new int[]{xl,yl+1});
-                        zmena=true;
+                if (yl + 1 < size && notContainsXY(list, xl, yl + 1)) {
+                    list.add(new int[]{xl, yl + 1, board[xl][yl + 1]});
+                    if (board[xl][yl + 1] == '0') {
+                        zeros.add(new int[]{xl, yl + 1});
                     }
                 }
-
-                if (yl - 1 >= 0 && !containsXY(list,xl,yl-1)) {
-                    list.add(new int[]{xl,yl-1,board[xl][yl-1]});
-                    if(board[xl][yl-1]=='0'){
-                        zeros.add(new int[]{xl,yl-1});
-                        zmena=true;
+                if (yl - 1 >= 0 && notContainsXY(list, xl, yl - 1)) {
+                    list.add(new int[]{xl, yl - 1, board[xl][yl - 1]});
+                    if (board[xl][yl - 1] == '0') {
+                        zeros.add(new int[]{xl, yl - 1});
                     }
                 }
-
             }
-        }while(zmena);
+        } while (zeros.size() != 0);
         zeros.clear();
         return list;
     }
 
-    private boolean containsXY(List<int[]> list,int x, int y){
-        for(int[] tmp:list){
-            if(tmp[0]==x && tmp[1]==y)
-                return true;
+    private boolean notContainsXY(List<int[]> list, int x, int y) {
+        for (int[] tmp : list) {
+            if (tmp[0] == x && tmp[1] == y)
+                return false;
         }
-        return false;
+        return true;
     }
 
-    public boolean addFlag(int x,int y){
-        flags.add(new int[]{x,y});
+    public boolean addFlag(int x, int y) {
+        flags.add(new int[]{x, y});
         return checkWin();
     }
-    public boolean removeFlag(int x,int y){
-        flags.remove(new int[]{x,y});
+
+    public boolean removeFlag(int x, int y) {
+        flags.remove(new int[]{x, y});
         return checkWin();
     }
-    private boolean checkWin(){
-        if(flags.size()!=mines)
+
+    private boolean checkWin() {
+        if (flags.size() != mines)
             return false;
         for (int[] flag : flags) {
             if (board[flag[0]][flag[1]] != 'X')
@@ -152,8 +136,8 @@ public class Logic {
         }
         return true;
     }
-    public char[][] getBoard() {
 
+    public char[][] getBoard() {
         return board;
     }
 }
